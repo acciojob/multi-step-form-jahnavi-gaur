bd319e878554e24acdca9f89c2c4c4aff084d964
Jahnavi <gaurjahnavi84@gmail.com>
2025-12-18 09:02:14 +0530

    third commit

--- src/components/Step.js
+++ src/components/Step.js
@@ -12,63 +12,27 @@ const Step = ({
     <>
       {step === 1 && (
         <div id="step1">
-          <h3>Customer Details</h3>
-
-          <input
-            id="first_name"
-            value={formData.first_name}
-            onChange={handleChange}
-          />
-
-          <input
-            id="last_name"
-            value={formData.last_name}
-            onChange={handleChange}
-          />
-
-          <button onClick={nextStep}>Next</button>
+          <input id="first_name" value={formData.first_name} onChange={handleChange} />
+          <input id="last_name" value={formData.last_name} onChange={handleChange} />
+          <button type="button" onClick={nextStep}>Next</button>
         </div>
       )}
 
       {step === 2 && (
         <div id="step2">
-          <h3>Car Details</h3>
-
-          <input
-            id="model"
-            value={formData.model}
-            onChange={handleChange}
-          />
-
-          <input
-            id="car_price"
-            value={formData.car_price}
-            onChange={handleChange}
-          />
-
-          <button onClick={prevStep}>Previous</button>
-          <button onClick={nextStep}>Next</button>
+          <input id="model" value={formData.model} onChange={handleChange} />
+          <input id="car_price" value={formData.car_price} onChange={handleChange} />
+          <button type="button" onClick={prevStep}>Previous</button>
+          <button type="button" onClick={nextStep}>Next</button>
         </div>
       )}
 
       {step === 3 && (
         <div id="step3">
-          <h3>Payment Details</h3>
-
-          <input
-            id="card_info"
-            value={formData.card_info}
-            onChange={handleChange}
-          />
-
-          <input
-            id="expiry_date"
-            value={formData.expiry_date}
-            onChange={handleChange}
-          />
-
-          <button onClick={prevStep}>Previous</button>
-          <button onClick={handleSubmit}>Submit</button>
+          <input id="card_info" value={formData.card_info} onChange={handleChange} />
+          <input id="expiry_date" value={formData.expiry_date} onChange={handleChange} />
+          <button type="button" onClick={prevStep}>Previous</button>
+          <button type="button" onClick={handleSubmit}>Submit</button>
         </div>
       )}
     </>
